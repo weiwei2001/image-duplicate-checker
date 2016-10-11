5048100f74e0d715594f38a743f870252a345f06
Weiwei ZHANG <zhangweiwei2001@gmail.com>
2016-10-11 22:17:03 +0200

    Fix exceptions; Improve map

--- src/main/java/fr/polyconseil/imageduplicatechecker/CheckerBuilder.java
+++ src/main/java/fr/polyconseil/imageduplicatechecker/CheckerBuilder.java
@@ -1,4 +1,5 @@
 package fr.polyconseil.imageduplicatechecker;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -27,11 +28,11 @@ import net.sf.json.JSONObject;
  *
  * <p>
  * When the user configures the project and enables this builder,
- * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
- * and a new {@link CheckerBuilder} is created. The created
- * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #excludedFolders})
- * to remember the configuration.
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked and a new
+ * {@link CheckerBuilder} is created. The created instance is persisted to the
+ * project configuration XML by using XStream, so this allows you to use
+ * instance fields (like {@link #excludedFolders}) to remember the
+ * configuration.
  *
  * <p>
  * When a build is performed, the {@link #perform} method will be invoked.
@@ -40,129 +41,139 @@ import net.sf.json.JSONObject;
  */
 public class CheckerBuilder extends Publisher implements SimpleBuildStep {
 
-    private final String excludedFolders;
-    private final String allowedFileExtensions;
-    private final String sourcePathPattern;
+	private final String excludedFolders;
+	private final String allowedFileExtensions;
+	private final String sourcePathPattern;
 
-    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
-    @DataBoundConstructor
-    public CheckerBuilder(String excludedFolders, String allowedFileExtensions, String sourcePathPattern) {
-        this.excludedFolders = excludedFolders;
-        this.allowedFileExtensions = allowedFileExtensions;
-        this.sourcePathPattern = sourcePathPattern;
-    }
+	// Fields in config.jelly must match the parameter names in the
+	// "DataBoundConstructor"
+	@DataBoundConstructor
+	public CheckerBuilder(String excludedFolders, String allowedFileExtensions, String sourcePathPattern) {
+		this.excludedFolders = excludedFolders;
+		this.allowedFileExtensions = allowedFileExtensions;
+		this.sourcePathPattern = sourcePathPattern;
+	}
 
+	public String getExcludedFolders() {
+		return excludedFolders;
+	}
 
-    public String getExcludedFolders() {
-        return excludedFolders;
-    }
+	public String getAllowedFileExtensions() {
+		return allowedFileExtensions;
+	}
 
-    public String getAllowedFileExtensions() {
-        return allowedFileExtensions;
-    }
-    
-    public String getSourcePathPattern() {
-        return sourcePathPattern;
-    }
+	public String getSourcePathPattern() {
+		return sourcePathPattern;
+	}
 
-    @Override
-    public void perform(Run<?,?> build, FilePath workspace, Launcher launcher, TaskListener listener) {
-        FindDuplicates.execute(this.getExcludedFolders(), this.getAllowedFileExtensions(), this.getSourcePathPattern(), workspace, listener);
-    }
+	@Override
+	public void perform(Run<?, ?> build, FilePath workspace, Launcher launcher, TaskListener listener) {
+		FindDuplicates.execute(this.getExcludedFolders(), this.getAllowedFileExtensions(), this.getSourcePathPattern(),
+				workspace, listener);
+	}
 
-    // Overridden for better type safety.
-    // If your plugin doesn't really define any property on Descriptor,
-    // you don't have to do this.
-    @Override
-    public BuildStepDescriptor<Publisher> getDescriptor() {
-        return (DescriptorImpl)super.getDescriptor();
-    }
+	// Overridden for better type safety.
+	// If your plugin doesn't really define any property on Descriptor,
+	// you don't have to do this.
+	@Override
+	public BuildStepDescriptor<Publisher> getDescriptor() {
+		return (DescriptorImpl) super.getDescriptor();
+	}
 
-    @Extension // This indicates to Jenkins that this is an implementation of an extension point.
-    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+	@Extension // This indicates to Jenkins that this is an implementation of an
+				// extension point.
+	public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
 
-        // TODO(wzhang): remove global config!
-        private String addPath;
+		// TODO(wzhang): remove global config!
+		private String addPath;
 
-        public DescriptorImpl() {
-            load();
-        }
+		public DescriptorImpl() {
+			load();
+		}
 
-        /**
-         * Performs on-the-fly validation of the form field 'name'.
-         *
-         * @param excludedFolders
-         *      This parameter receives the value that the user has typed.
-         * @param allowedFileExtensions
-         *      This parameter receives the value that the user has typed.
-         * @param sourcePathPattern
-         *      This parameter receives the value that the user has typed.
-         * @return
-         *      Indicates the outcome of the validation. This is sent to the browser.
-         *      <p>
-         *      Note that returning {@link FormValidation#error(String)} does not
-         *      prevent the form from being saved. It just means that a message
-         */
-        public FormValidation doCheckName(@QueryParameter String excludedFolders, @QueryParameter String allowedFileExtensions, @QueryParameter String sourcePathPattern)
-                throws IOException, ServletException {
-            if (excludedFolders.isEmpty() || allowedFileExtensions.isEmpty())
-                return FormValidation.error("Please supply a path to directory to find duplicate files in.");
-            if (excludedFolders.length() < 2 || allowedFileExtensions.length() < 2)
-                return FormValidation.warning("Isn't the folder path too short?");
-            File excludedFoldersFile = new File(excludedFolders);
-            File allowedFileExtensionsFile = new File(allowedFileExtensions);
-            if (!excludedFoldersFile.isDirectory() || !allowedFileExtensionsFile.isDirectory()) {
-                return FormValidation.error("Supplied directory (" + excludedFolders  + ") does not exist.");
-            }
-            if (sourcePathPattern.isEmpty())
-                return FormValidation.error("Please define a folder/file to write images duplacate report.");
-            return FormValidation.ok();
-        }
+		/**
+		 * Performs on-the-fly validation of the form field 'name'.
+		 *
+		 * @param excludedFolders
+		 *            This parameter receives the value that the user has typed.
+		 * @param allowedFileExtensions
+		 *            This parameter receives the value that the user has typed.
+		 * @param sourcePathPattern
+		 *            This parameter receives the value that the user has typed.
+		 * @return Indicates the outcome of the validation. This is sent to the
+		 *         browser.
+		 *         <p>
+		 *         Note that returning {@link FormValidation#error(String)} does
+		 *         not prevent the form from being saved. It just means that a
+		 *         message
+		 */
+		public FormValidation doCheckName(@QueryParameter String excludedFolders,
+				@QueryParameter String allowedFileExtensions, @QueryParameter String sourcePathPattern)
+				throws IOException, ServletException {
+			if (excludedFolders.isEmpty() || allowedFileExtensions.isEmpty())
+				return FormValidation.error("Please supply a path to directory to find duplicate files in.");
+			if (excludedFolders.length() < 2 || allowedFileExtensions.length() < 2)
+				return FormValidation.warning("Isn't the folder path too short?");
+			File excludedFoldersFile = new File(excludedFolders);
+			File allowedFileExtensionsFile = new File(allowedFileExtensions);
+			if (!excludedFoldersFile.isDirectory() || !allowedFileExtensionsFile.isDirectory()) {
+				return FormValidation.error("Supplied directory (" + excludedFolders + ") does not exist.");
+			}
+			if (sourcePathPattern.isEmpty())
+				return FormValidation.error("Please define a folder/file to write images duplacate report.");
+			return FormValidation.ok();
+		}
 
-        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-            // Indicates that this builder can be used with all kinds of project types
-            return true;
-        }
+		public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+			// Indicates that this builder can be used with all kinds of project
+			// types
+			return true;
+		}
 
-        /**
-         * This human readable name is used in the configuration screen.
-         */
-        public String getDisplayName() {
-            return "Check duplicate images";
-        }
+		/**
+		 * This human readable name is used in the configuration screen.
+		 */
+		public String getDisplayName() {
+			return "Check duplicate images";
+		}
 
-        @Override
-        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            // To persist global configuration information,
-            // set that to properties and call save().
-            addPath = nullify(formData.getString("addPath"));
-            // ^Can also use req.bindJSON(this, formData);
-            //  (easier when there are many fields; need set* methods for this, like setUseFrench)
-            save();
-            return super.configure(req,formData);
-        }
+		@Override
+		public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+			// To persist global configuration information,
+			// set that to properties and call save().
+			addPath = nullify(formData.getString("addPath"));
+			// ^Can also use req.bindJSON(this, formData);
+			// (easier when there are many fields; need set* methods for this,
+			// like setUseFrench)
+			save();
+			return super.configure(req, formData);
+		}
 
-        private String nullify(String v) {
-            if(v!=null && v.length()==0)    v=null;
-            return v;
-        }
+		private String nullify(String v) {
+			if (v != null && v.length() == 0)
+				v = null;
+			return v;
+		}
 
-        /**
-         * This method returns true if the global configuration says we should speak French.
-         *
-         * The method name is bit awkward because global.jelly calls this method to determine
-         * the initial state of the checkbox by the naming convention.
-         */
-        public void setAddPath(String addPath) {
-            this.addPath = addPath;
-        }
-        public String getAddPath() {
-            return addPath;
-        }
-    }
+		/**
+		 * This method returns true if the global configuration says we should
+		 * speak French.
+		 *
+		 * The method name is bit awkward because global.jelly calls this method
+		 * to determine the initial state of the checkbox by the naming
+		 * convention.
+		 */
+		public void setAddPath(String addPath) {
+			this.addPath = addPath;
+		}
 
-    @Override
-    public BuildStepMonitor getRequiredMonitorService() {
-        return BuildStepMonitor.NONE;
-    }
+		public String getAddPath() {
+			return addPath;
+		}
+	}
+
+	@Override
+	public BuildStepMonitor getRequiredMonitorService() {
+		return BuildStepMonitor.NONE;
+	}
 }
--- src/main/java/fr/polyconseil/imageduplicatechecker/FindDuplicates.java
+++ src/main/java/fr/polyconseil/imageduplicatechecker/FindDuplicates.java
@@ -12,10 +12,10 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.io.FilenameUtils;
 
@@ -29,128 +29,128 @@ import hudson.model.TaskListener;
  * http://stackoverflow.com/questions/1741545/java-calculate-sha-256-hash-of-large-file-efficiently
  */
 public class FindDuplicates {
-    private static MessageDigest md;
-    private static List<String> splitExcludedFolder;
-    private static List<String> splitAllowedFileExtensions;
-    static {
-        try {
-            md = MessageDigest.getInstance("SHA-512");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("cannot initialize SHA-512 hash function", e);
-        }
-    }
-
-    public static void find(Map<String, List<String>> duplicatesMap, File directory, boolean leanAlgorithm) throws Exception  {
-        for (File child : directory.listFiles()) {
-    		if (splitExcludedFolder.contains(FilenameUtils.getBaseName(child.getName()))) {
-    			continue;
-    		}
-        	if (child.isDirectory()) {
-                find(duplicatesMap, child, leanAlgorithm);
-            } else {
-                try {
-                String extension = FilenameUtils.getExtension(child.getName());
-        		if (splitAllowedFileExtensions.contains(extension)) {
-                    String hash = leanAlgorithm ? makeHashLean(child) : makeHashQuick(child);
-                    List<String> filenameList = duplicatesMap.get(hash);
-                    if (filenameList == null) {
-                        filenameList = new LinkedList<String>();
-                        duplicatesMap.put(hash, filenameList);
-                    }
-                    filenameList.add(child.getAbsolutePath());
-                }
-                } catch (IOException e) {
-                    throw new RuntimeException("cannot read file " + child.getAbsolutePath(), e);
-                }
-            }
-        }
-    }
-
-    /*
-     * quick but memory hungry (might like to run with java -Xmx2G or the like to increase heap space if RAM available)
-     */
-    public static String makeHashQuick(File infile) throws Exception {
-        FileInputStream fin = new FileInputStream(infile);
-        byte data[] = new byte[(int) infile.length()];
-        fin.read(data);
-        fin.close();
-        String hash = new BigInteger(1, md.digest(data)).toString(16);
-        return hash;
-    }
-
-    /*
-     * slower but memory efficient  -- you might like to play with the size defined by "buffSize"
-     */
-    public static String makeHashLean(File infile) throws Exception {
-        RandomAccessFile file = new RandomAccessFile(infile, "r");
-
-        int buffSize = 16384;
-        byte[] buffer = new byte[buffSize];
-        long read = 0;
-
-        // calculate the hash of the whole file for the test
-        long offset = file.length();
-        int unitsize;
-        while (read < offset) {
-            unitsize = (int) (((offset - read) >= buffSize) ? buffSize
-            		: (offset - read));
-            file.read(buffer, 0, unitsize);
-            md.update(buffer, 0, unitsize);
-            read += unitsize;
-        }
-
-        file.close();
-        String hash = new BigInteger(1, md.digest()).toString(16);
-        return hash;
-    }
-    
-    public static void execute(String excludedFolder, String allowedFileExtensions, String sourcePathPattern, FilePath workspace, TaskListener listener) {
-    	listener.getLogger().println("Starting scanning folder: " + workspace + "!");
-    	splitExcludedFolder = Arrays.asList(excludedFolder.trim().split("\\s+"));
-    	splitAllowedFileExtensions = Arrays.asList(allowedFileExtensions.trim().split("\\s+"));
-    	File dir = new File(workspace.getRemote());
-        Map<String, List<String>> duplicatesMap = new ConcurrentHashMap<String, List<String>>();
-        try {
-            FindDuplicates.find(duplicatesMap, dir, true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        
-        // for Java 8: duplicatesMap.keySet().removeIf(e->(duplicatesMap.get(e).size()<2));
-        for (String key : duplicatesMap.keySet()) {
-            if (duplicatesMap.get(key).size() < 2) {
-            	duplicatesMap.remove(key);
-            }
-        }
-        generateReport(sourcePathPattern, workspace, duplicatesMap);
-        listener.getLogger().println(duplicatesMap.size() + " results generated in file: " + sourcePathPattern);
-    }
-
-	private static void generateReport(String sourcePathPattern, FilePath workspace, Map<String, List<String>> duplicatesMap) {
-        FilePath f = workspace.child(sourcePathPattern);
-        PrintWriter w;
+	private static MessageDigest md;
+	private static List<String> splitExcludedFolder;
+	private static List<String> splitAllowedFileExtensions;
+	static {
 		try {
-			w = new PrintWriter(
-			    new BufferedWriter(
-			        new OutputStreamWriter(
-			            f.write(), "UTF-8")));
-            for (List<String> filenameList : duplicatesMap.values()) {
-            	w.println("Found same image in folders:");
-	        	for (String filename : filenameList) {
-	                w.println(filename);
-	        	}
-	        	w.println("---------------------------------------------------------");
-            }
-        	w.close();
+			md = MessageDigest.getInstance("SHA-512");
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException("cannot initialize SHA-512 hash function", e);
+		}
+	}
+
+	public static void find(Map<String, List<String>> duplicatesMap, File directory, boolean leanAlgorithm)
+			throws Exception {
+		for (File child : directory.listFiles()) {
+			if (splitExcludedFolder.contains(FilenameUtils.getBaseName(child.getName()))) {
+				continue;
+			}
+			if (child.isDirectory()) {
+				find(duplicatesMap, child, leanAlgorithm);
+			} else {
+				try {
+					String extension = FilenameUtils.getExtension(child.getName());
+					if (splitAllowedFileExtensions.contains(extension)) {
+						String hash = leanAlgorithm ? makeHashLean(child) : makeHashQuick(child);
+						List<String> filenameList = duplicatesMap.get(hash);
+						if (filenameList == null) {
+							filenameList = new LinkedList<String>();
+							duplicatesMap.put(hash, filenameList);
+						}
+						filenameList.add(child.getAbsolutePath());
+					}
+				} catch (IOException e) {
+					throw new RuntimeException("cannot read file " + child.getAbsolutePath(), e);
+				}
+			}
+		}
+	}
+
+	/*
+	 * quick but memory hungry (might like to run with java -Xmx2G or the like
+	 * to increase heap space if RAM available)
+	 */
+	public static String makeHashQuick(File infile) throws Exception {
+		FileInputStream fin = new FileInputStream(infile);
+		byte data[] = new byte[(int) infile.length()];
+		fin.read(data);
+		fin.close();
+		String hash = new BigInteger(1, md.digest(data)).toString(16);
+		return hash;
+	}
+
+	/*
+	 * slower but memory efficient -- you might like to play with the size
+	 * defined by "buffSize"
+	 */
+	public static String makeHashLean(File infile) throws Exception {
+		RandomAccessFile file = new RandomAccessFile(infile, "r");
+
+		int buffSize = 16384;
+		byte[] buffer = new byte[buffSize];
+		long read = 0;
+
+		// calculate the hash of the whole file for the test
+		long offset = file.length();
+		int unitsize;
+		while (read < offset) {
+			unitsize = (int) (((offset - read) >= buffSize) ? buffSize : (offset - read));
+			file.read(buffer, 0, unitsize);
+			md.update(buffer, 0, unitsize);
+			read += unitsize;
+		}
+
+		file.close();
+		String hash = new BigInteger(1, md.digest()).toString(16);
+		return hash;
+	}
+
+	public static void execute(String excludedFolder, String allowedFileExtensions, String sourcePathPattern,
+			FilePath workspace, TaskListener listener) {
+		listener.getLogger().println("Starting scanning folder: " + workspace + "!");
+		splitExcludedFolder = Arrays.asList(excludedFolder.trim().split("\\s+"));
+		splitAllowedFileExtensions = Arrays.asList(allowedFileExtensions.trim().split("\\s+"));
+		File dir = new File(workspace.getRemote());
+		Map<String, List<String>> duplicatesMap = new HashMap<String, List<String>>();
+		try {
+			FindDuplicates.find(duplicatesMap, dir, true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Wait for Java 8:
+		// duplicatesMap.keySet().removeIf(e->(duplicatesMap.get(e).size()<2));
+		Map<String, List<String>> uniqueDuplicatesMap = new HashMap<String, List<String>>();
+		for (String key : duplicatesMap.keySet()) {
+			if (duplicatesMap.get(key).size() > 1) {
+				uniqueDuplicatesMap.put(key, duplicatesMap.get(key));
+			}
+		}
+		generateReport(sourcePathPattern, workspace, uniqueDuplicatesMap);
+		listener.getLogger().println(uniqueDuplicatesMap.size() + " results generated in file: " + sourcePathPattern);
+	}
+
+	private static void generateReport(String sourcePathPattern, FilePath workspace,
+			Map<String, List<String>> duplicatesMap) {
+		FilePath f = workspace.child(sourcePathPattern);
+		PrintWriter w;
+		try {
+			w = new PrintWriter(new BufferedWriter(new OutputStreamWriter(f.write(), "UTF-8")));
+			for (List<String> filenameList : duplicatesMap.values()) {
+				w.println("Found same image in folders:");
+				for (String filename : filenameList) {
+					w.println(filename);
+				}
+				w.println("---------------------------------------------------------");
+			}
+			w.close();
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException("cannot read unsupported encoding in " + sourcePathPattern, e);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException("cannot read file " + sourcePathPattern + " in " + workspace.getRemote(), e);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new RuntimeException("cannot write file " + sourcePathPattern + " in " + workspace.getRemote(), e);
 		}
 	}
 }
